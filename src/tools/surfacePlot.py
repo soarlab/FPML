9ca3bbb2fc45d4170c0bbbd3913405d8fc642328
Roki <salvia.rocco@gmail.com>
2018-01-26 01:00:27 -0700

    Corrections to paper and graphs

--- src/tools/surfacePlot.py
+++ src/tools/surfacePlot.py
@@ -9,7 +9,7 @@ from matplotlib.pyplot import draw
 
 
 
-matplotlib.rcParams.update({'font.size': 17})
+matplotlib.rcParams.update({'font.size': 10})
 
 def splitDotValue(column):
 	values=column.split(":")
@@ -24,106 +24,112 @@ def getAccuracy(val):
 	return acc
 	
 data=[]
+algoritmh="AP"
+folder="/home/roki/GIT/src/final/splice/FLEX/"
 
-fileNames=["/home/roki/GIT/src/final/fourclass/MPFR/part2APTest.txt",
-		   "/home/roki/GIT/src/final/fourclass/FLEX/part2APTest.txt"]
-maxi=0
-mini=1.0
-for fileName in fileNames:
-	with open(fileName) as f:
-		data=f.readlines()
-	for line in data:
-		columns=line.split("$")
-		if getAccuracy(columns[6])>0:
-			maxi=max(maxi,getAccuracy(columns[6]))
-			mini=min(mini,getAccuracy(columns[6]))
-
-for fileName in fileNames:
-	with open(fileName) as f:
-		data=f.readlines()
-	
-	labelsComputation=[]
-	labelsReading=[]
-	labelsTest=[]
-	
-	for line in data:
-		columns=line.split("$")
-		if "exc" not in line:
-			if columns[1]!="" and columns[1] not in labelsReading: 
-				labelsReading.append(columns[1])
-			if columns[2]!="" and columns[2] not in labelsComputation: 
-				labelsComputation.append(columns[2])
-			if columns[3]!="" and columns[3] not in labelsTest: 
-				labelsTest.append(columns[3])
-	
-	matrix=numpy.zeros((len(labelsReading),len(labelsComputation),len(labelsTest)))
-	
-	for line in data:
-		columns=line.split("$")
-		if getAccuracy(columns[6])!=0:
-			matrix[labelsReading.index(columns[1])][labelsComputation.index(columns[2])][labelsTest.index(columns[3])]=getAccuracy(columns[6])
-
-	labelHiddenX=range(0,len(labelsComputation),5)
-	labelHiddenY=range(0,len(labelsReading),5)
-	labelHiddenZ=range(0,len(labelsTest),1)
-	
-	for i,val in enumerate(labelsReading):
-		if i not in labelHiddenY and i!=len(labelsReading)-1:
-			labelsReading[i]=""
-	
-	for i,val in enumerate(labelsComputation): 
-		if i not in labelHiddenX and i!=len(labelsComputation)-1:
-			labelsComputation[i]=""
-	
-	for i,val in enumerate(labelsTest): 
-		if i not in labelHiddenZ and i!=len(labelHiddenZ)-1:
-			labelsTest[i]=""
-	
-	indexX=range(0,len(labelsReading),1)
-	indexY=range(0,len(labelsComputation),1)
-	indexZ=range(0,len(labelsTest),1)
-	
-	X, Y = np.meshgrid(indexX, indexY)
-	
-	fig = plt.figure(num=None, dpi=80, facecolor='w', edgecolor='k')
-	
-	norm = matplotlib.colors.Normalize(vmin = mini, vmax = maxi, clip = False)
-	
-	plt.subplots_adjust(top = 0.98, bottom = 0.02, right = 0.92, left = 0, hspace = 0.1, wspace = 0.1)
+for tt in ["Train","Test"]:
+	for indFile in range(1,5): 
+		fileNames=["/home/roki/GIT/src/final/splice/FLEX/part"+str(indFile)+algoritmh+tt+".txt", 
+				   "/home/roki/GIT/src/final/splice/MPFR/part"+str(indFile)+algoritmh+tt+".txt"]
+		print indFile
+		maxi=0
+		mini=1.0
+		for fileName in fileNames:
+			with open(fileName) as f:
+				data=f.readlines()
+			for line in data:
+				columns=line.split("$")
+				if getAccuracy(columns[6])>0:
+					maxi=max(maxi,getAccuracy(columns[6]))
+					mini=min(mini,getAccuracy(columns[6]))
 		
-	Z=numpy.zeros((len(labelsReading),len(labelsComputation)))
-
-	for i in range(0,len(labelsTest)):
-		for k in indexX:
-			for l in indexY:
-				Z[k][l]=matrix[k][l][i]
-		#Z[Z<0.5]=np.NaN
-		ax = fig.add_subplot(3,4,i+1, projection='3d')
-		ax.view_init(90,0)
-		ax.set_zlim(mini, maxi)
-		ax.set_xticks(indexX)
-		ax.set_xticklabels(labelsReading,fontsize=14)
-		ax.set_yticks(indexY)
-		ax.set_yticklabels(labelsComputation,fontsize=14)
-		#ax.set_zticklabels([],fontsize=14)
-		ax.set_title("Test Precision:"+str(labelsTest[i]),fontsize=14)
-		ax.tick_params(axis='both', which='major', pad=12)
-		im=ax.plot_surface(X, Y, Z, cmap="cool", antialiased=True,norm=norm,alpha=1)
-	
-	cbar_ax = fig.add_axes([0.95, 0.05, 0.01, 0.9])
-	cbar_ax.set_yticklabels(['< -1', '0', '> 1'])
-	fig.colorbar(im,cax=cbar_ax)
-	title=fileName.split("/")[-1].split(".")[0]
-	
-	#fileName.split("/")[-3]+"\n"+
-	#if "AP" in fileName:
-	#	title=title+"AP"
-	#if "SVM" in fileName:
-	#	title=title+"SVM"
-	#if "P" in fileName:
-	#	title=title+"P"
-
-	plt.title(title),  
-	draw()
+		for fileName in fileNames:
+			with open(fileName) as f:
+				data=f.readlines()
+			
+			labelsComputation=[]
+			labelsReading=[]
+			labelsTest=[]
+			
+			for line in data:
+				columns=line.split("$")
+				if "exc" not in line:
+					if columns[1]!="" and columns[1] not in labelsReading: 
+						labelsReading.append(columns[1])
+					if columns[2]!="" and columns[2] not in labelsComputation: 
+						labelsComputation.append(columns[2])
+					if columns[3]!="" and columns[3] not in labelsTest: 
+						labelsTest.append(columns[3])
+			
+			matrix=numpy.zeros((len(labelsTest),len(labelsReading),len(labelsComputation)))
+			
+			for line in data:
+				columns=line.split("$")
+				if getAccuracy(columns[6])!=0:
+					matrix[labelsTest.index(columns[3])][labelsReading.index(columns[1])][labelsComputation.index(columns[2])]=getAccuracy(columns[6])
+			
+			labelHiddenX=[0,len(labelsComputation)-1]
+			labelHiddenY=[0,len(labelsReading)-1]
+			
+			for i,val in enumerate(labelsReading):
+				if i not in labelHiddenX:
+					labelsReading[i]=""
+			
+			for i,val in enumerate(labelsComputation): 
+				if i not in labelHiddenY:
+					labelsComputation[i]=""
+			
+			indexY=range(0,len(labelsReading),1)
+			indexX=range(0,len(labelsComputation),1)
+			indexZ=range(0,len(labelsTest),1)
+			
+		
+			X, Y = np.meshgrid(indexX,indexY)
+		
+			fig = plt.figure(num=None, dpi=100, facecolor='w', edgecolor='k')
+			
+			print mini
+			
+			print maxi
+			
+			norm = matplotlib.colors.Normalize(vmin = mini, vmax = maxi, clip = False)
+			
+			plt.subplots_adjust(top = 0.98, bottom = 0.02, right = 0.92, left = 0, hspace = 0.1, wspace = 0.1)
+		
+			for i in range(0,len(labelsTest)):
+				Z=matrix[i][:][:]
+				#Z[Z<0.5]=np.NaN
+				ax = fig.add_subplot(3,4,i+1, projection='3d')
+				ax.view_init(30,-20)
+				ax.set_zlim(mini, maxi)
+				ax.set_xticks(indexY)
+				ax.set_xticklabels(labelsReading)
+				ax.set_yticks(indexX)
+				ax.set_yticklabels(labelsComputation)
+				#ax.set_zticklabels([],fontsize=14)
+				ax.set_title("Test Precision:"+str(labelsTest[i]))
+				ax.set_xlabel("R")
+				ax.xaxis.labelpad = -8
+				ax.set_ylabel("C")
+				ax.yaxis.labelpad = -8
+				ax.tick_params(axis='both', which='major', pad=3)
+				im=ax.plot_surface(Y, X, Z, cmap="cool", antialiased=True,norm=norm,alpha=1)
+				
+			cbar_ax = fig.add_axes([0.95, 0.05, 0.01, 0.9])
+			cbar_ax.set_yticklabels(['< -1', '0', '> 1'])
+			fig.colorbar(im,cax=cbar_ax)
+			title=fileName.split("/")[-2]+fileName.split("/")[-1].split(".")[0]
+			
+			#fileName.split("/")[-3]+"\n"+
+			#if "AP" in fileName:
+			#	title=title+"AP"
+			#if "SVM" in fileName:
+			#	title=title+"SVM"
+			#if "P" in fileName:
+			#	title=title+"P"
+		
+			plt.title(title),
+			fig.canvas.set_window_title(title)  
+			draw()
 plt.show()
 	
